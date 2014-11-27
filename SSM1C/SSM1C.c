73be0ac1d69ba8d2c6fa236ce81d79a60a4b6e1c
BelegUS <belegussuicune@gmail.com>
2014-11-28 00:10:24 +0100

    Code rewrite started. Get Subaru Query Bytes from BT function added.

--- SSM1C/SSM1C.c
+++ SSM1C/SSM1C.c
@@ -14,85 +14,99 @@
 #include "uart.h"
 
 
-char* readFromECU(unsigned char msb, unsigned char lsb, char* buffer)
+char* queryEcuCommand(unsigned char msb, unsigned char lsb, char* buffer4)
 {
 
-	buffer[0]=(char)0x78;
-	buffer[1]=(char)msb;
-	buffer[2]=(char)lsb;
-	buffer[3]=(char)0x00;
+	buffer4[0]=(char)0x78;
+	buffer4[1]=(char)msb;
+	buffer4[2]=(char)lsb;
+	buffer4[3]=(char)0x00;
 	
-	return buffer;
+	return buffer4;
 }
 
-char* readRomId(char* buffer)
+char* queryRomIdCommand(char* buffer4)
 {
 
-	buffer[0]=(char)0x00;
-	buffer[1]=(char)0x46;
-	buffer[2]=(char)0x48;
-	buffer[3]=(char)0x49;
+	buffer4[0]=(char)0x00;
+	buffer4[1]=(char)0x46;
+	buffer4[2]=(char)0x48;
+	buffer4[3]=(char)0x49;
 	
-	return buffer;
+	return buffer4;
 }
+
+int getQueryAddressBytes(char* buffer2)
+{
+	unsigned int tempChar = UART_NO_DATA;	//Helper temp buffer for chars. Empty at start
+	
+	/*Get chars from BT until correct begining char (STX)
+	We do not worry about being stuck here, Telephone
+	will feed us with data*/
+	while(tempChar != (char)0x02) {
+		tempChar = uart1_getc();
+	}
+	tempChar = UART_NO_DATA;	//Clear the temp buffer.
+	
+	/* Get chars from BT until data arrives
+	We do not worry again, it will arrive.
+	This data will be treated as MSB of Subaru ECU Query*/
+	while(tempChar == UART_NO_DATA) {
+		tempChar = uart1_getc();
+	}
+	buffer2[0] = (char)tempChar;	//Store MSB in buffer
+	tempChar = UART_NO_DATA;	//Clear the temp buffer.
+	
+	/* Get chars from BT until data arrives
+	We do not worry again, it will arrive.
+	This data will be treated as LSB of Subaru ECU Query*/
+	while(tempChar == UART_NO_DATA) {
+		tempChar = uart1_getc();
+	}
+	buffer2[1] = (char)tempChar;	//Store LSB in buffer
+	tempChar = UART_NO_DATA;	//Clear the temp buffer.	
+	
+	/* Get chars from BT until data arrives
+	We do not worry again, it will arrive.
+	This char (ETB) will be used to sanity check
+	the whole chunk transmission*/
+	while(tempChar == UART_NO_DATA) {
+		tempChar = uart1_getc();
+	}
+	if(tempChar == (char)0x17) {
+		return 1;	//Everything went OK, we can use data from Buffer
+	} else {
+		return 0;	//Something went wrong, data in Buffer isn't OK
+	}
+	
+} 
 
 
 int main(void)
 {
 	unsigned int c = UART_NO_DATA;	//Buffer for things from Console
 	unsigned int s = UART_NO_DATA;	//Buffer for things from Subaru
-	unsigned char standardBuffer[4];
-	unsigned char bigBuffer[60];
-	char testBuffer[] = { (unsigned char)0x78, (unsigned char)0x00, (unsigned char)0x09, (unsigned char)0x00 };
-	unsigned char testBuffer2[] = { 'c', 'h', 'a', 'r' };
-	//uart1_init(UART_BAUD_SELECT(1953,F_CPU));	//UART1, Komputer
-	uart1_init(UART_BAUD_SELECT(9600,F_CPU));	//UART1, Telefon BT
-	uart_init(UART_BAUD_SELECT(1953,F_CPU));	//UART0, Samochód
+	unsigned char queryBuffer[4];	//Buffer for complete Subaru Query
+	unsigned char addressBuffer[2];	//Buffer for MSB and LSB to create Subaru Query
+
+	uart_init(UART_BAUD_SELECT(1953,F_CPU));	//UART0, Subaru SSM-1
+	uart1_init(UART_BAUD_SELECT(9600,F_CPU));	//UART1, Bluetooth Module
+	
 	sei(); 
+	
     while(1)
     {
- /*
-         * Get received character from ringbuffer
-         * uart_getc() returns in the lower byte the received character and 
-         * in the higher byte (bitmask) the last receive error
-         * UART_NO_DATA is returned when no data is available.
-         *
-         */
+		/* Flush UART buffers */
 		uart_flush();
 		uart1_flush();
 		
-		while(c == UART_NO_DATA)
+		/* Wait for synchronization (SYN) char from Bluetooth */
+		while(c != (char)0x16)
 		{
 			c = uart1_getc();
 		}
-            /*
-             * new data available from UART
-             * check for Frame or Overrun error
-             */
-            if ( c & UART_FRAME_ERROR )
-            {
-                /* Framing Error detected, i.e no stop bit detected */
-                uart1_puts("PC UART Frame Error: ");
-            }
-            if ( c & UART_OVERRUN_ERROR )
-            {
-                /* 
-                 * Overrun, a character already present in the UART UDR register was 
-                 * not read by the interrupt handler before the next character arrived,
-                 * one or more received characters have been dropped
-                 */
-                uart1_puts("PC UART Overrun Error: ");
-            }
-            if ( c & UART_BUFFER_OVERFLOW )
-            {
-                /* 
-                 * We are not reading the receive buffer fast enough,
-                 * one or more received character have been dropped 
-                 */
-                uart1_puts("PC Buffer overflow error: ");
-            }
-			else{
-				uart_putsubaru( testBuffer );								
+
+		uart_putsubaru( testBuffer );		//Zabezpiecz to - co jeœli auto nie odpowie?						
 	
 			while(1) {
 					while(s == UART_NO_DATA)
@@ -123,7 +137,7 @@ int main(void)
 				
 
 			}
-		}
+	
 			c = UART_NO_DATA;
 		}
 
